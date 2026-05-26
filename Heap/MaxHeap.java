12a5473bc5f7d52184e69ae7db10700a8cbe8d73
Rajvardhan Patil <rp0887856@gmail.com>
2026-05-26 17:32:26 +0530

    Heap Implementation: Deletion

--- Heap/MaxHeap.java
+++ Heap/MaxHeap.java
@@ -12,6 +12,15 @@ class MaxHeap {
         return (i-1)/2;
     }
 
+    private int leftChild(int i) {
+        return 2 * i + 1;
+    }
+
+    // Get right child index
+    private int rightChild(int i) {
+        return 2 * i + 2;
+    }
+
     private void swap(int i, int j){
         int temp = heap[i];
         heap[i] = heap[j];
@@ -33,6 +42,35 @@ class MaxHeap {
 
     }
 
+    public int delete(){
+        if (size == 0) {
+            System.out.println("Heap is empty");
+            return -1;
+        }
+        int root = heap[0];
+        heap[0] = heap[size-1];
+        size--;
+        heapify(0);
+        return root;
+    }
+
+    public void heapify(int i){
+        int curr = i;
+        int left = leftChild(i);
+        int right = rightChild(i);
+
+        if(left < size && heap[curr] < heap[leftChild(i)]){
+            curr = left;
+        }
+        if(right < size && heap[curr] < heap[rightChild(i)]){
+            curr = right;
+        }
+        if(curr != i){
+            swap(curr, i);
+            heapify(curr);
+        }
+    }
+
     public void printHeap() {
         for (int i = 0; i < size; i++) {
             System.out.print(heap[i] + " ");
@@ -52,6 +90,12 @@ class MaxHeap {
         h.insert(60);
 
         System.out.println("Heap:");
+        h.printHeap();
+
+        System.out.println(h.delete());
+        System.out.println(h.delete());
+        
+        System.out.println("Heap After Deletion:");
         h.printHeap();
     }
 }
